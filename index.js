c59129542c5d22bf4b86ae00964dba3751139a94
vuonghome <vuonghome@xxx.com>
2020-10-12 01:21:46 +0700

    sino stub chain

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const MongoClient = require("mongodb").MongoClient;
 // Connection URL
-const url = "mongodb://localhost:27017";
+const url = "mongodb://localhost:27018";
 // Database Name
 const dbName = "myproject";
 module.exports = function () {
   // return 5;
   // Use connect method to connect to the server
   return new Promise((res, rej) => {
-    MongoClient.connect(url, { useUnifiedTopology: true }, function (
-      err,
-      client
-    ) {
-      console.log("Connected successfully to server");
+      let client;
+    MongoClient.connect(url, { useUnifiedTopology: true })
+    .then((xxx)=>{
+        client = xxx;
+        console.log("Connected successfully to server");
 
       const db = client.db(dbName);
       const collection = db.collection("documents");
-      return collection.insertOne({ a: 1 }, function (err, result) {
-        console.log("Inserted 1 documents into the collection");
+      return collection.insertOne({ a: 1 });
+    })
+    .then((c)=>{
         client.close();
+        console.log("Inserted 1 documents into the collection");
         res(5);
-      });
-    });
+
+    })
+    .catch(err=>{
+        res(0);
+    })
   });
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,65 @@
-const expect = require('chai').expect;
+const expect = require("chai").expect;
 var sinon = require("sinon");
-const index = require('../index');
-describe('Test index', function(){
-    it(' a should return 5', async function(){
-        const x = await index();
-        console.log(x);
-        expect(x).to.be.eql(5);
-      })
-})
+const MongoClient = require("mongodb").MongoClient;
+const index = require("../index");
+
+// some fake object that maybe looks like what you have
+
+describe("Test index", function () {
+  describe("Test index success", function () {
+    let fakeDbSuccess = {
+      db() {
+        return this;
+      },
+      collection() {
+        return this;
+      },
+      insertOne() {
+        return this;
+      },
+      close() {
+        console.log("close db");
+      },
+    };
+    before(function () {
+      sinon
+        .stub(MongoClient, "connect")
+        .returns(Promise.resolve(fakeDbSuccess));
+    });
+    after(function () {
+      sinon.restore();
+    });
+    it(" a should return 5", async function () {
+      const x = await index();
+      console.log(x);
+      expect(x).to.be.eql(5);
+    });
+  });
+  describe("Test index fail", function () {
+    let fakeDbError = {
+      db() {
+        return this;
+      },
+      collection() {
+        throw Error("cannot connect to db");
+      },
+      insertOne() {
+        return this;
+      },
+      close() {
+        console.log("close db");
+      },
+    };
+    before(function () {
+      sinon.stub(MongoClient, "connect").returns(Promise.resolve(fakeDbError));
+    });
+    after(function () {
+      sinon.restore();
+    });
+    it(" a should return 0", async function () {
+      const x = await index();
+      console.log(x);
+      expect(x).to.be.eql(0);
+    });
+  });
+});
